c9a2295a4fc3cab242880b4ab9f9b7434f2bfb7a
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-11-17 15:12:58 +0000

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,79 @@
+variable "tags" {}
+
+variable "cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "instance_tenancy" {
+  type    = string
+  default = "default"
+}
+
+variable "enable_dns_hostnames" {
+  type    = bool
+  default = false
+}
+
+variable "enable_dns_support" {
+  type    = bool
+  default = true
+}
+
+variable "dhcp_options_domain_name" {
+  type    = string
+  default = ""
+}
+
+variable "dhcp_options_domain_name_servers" {
+  type    = list(string)
+  default = ["AmazonProvidedDNS"]
+}
+
+variable "map_public_ip_on_launch" {
+  type    = bool
+  default = false
+}
+
+variable "public_subnet_assign_ipv6_address_on_creation" {
+  type    = bool
+  default = false
+}
+
+variable "default_security_group_name" {
+  type    = string
+  default = "default"
+}
+
+variable "default_security_group_ingress" {
+  type    = list(map(string))
+  default = null
+}
+
+variable "default_security_group_egress" {
+  type    = list(map(string))
+  default = null
+}
+
+variable "flow_log_log_format" {
+  type    = string
+  default = null
+}
+
+variable "flow_log_traffic_type" {
+  description = "The type of traffic to log. Values: ACCEPT, REJECT, ALL."
+  type        = string
+  default     = "ALL"
+}
+
+
+variable "flow_log_cloudwatch_log_group_retention_in_days" {
+  description = "Number of days you want to retain log events."
+  type        = number
+  default     = null
+}
+
+variable "flow_log_cloudwatch_log_group_kms_key_id" {
+  type    = string
+  default = null
+}
---
+++ vpc-flow-logs.tf
@@ -0,0 +1,67 @@
+resource "aws_flow_log" "this" {
+  log_destination_type     = "cloud-watch-logs"
+  log_destination          = aws_cloudwatch_log_group.flow_log.arn
+  log_format               = var.flow_log_log_format
+  iam_role_arn             = aws_iam_role.vpc_flow_log_cloudwatch.arn
+  traffic_type             = var.flow_log_traffic_type
+  vpc_id                   = aws_vpc.main.id
+  max_aggregation_interval = 600
+
+  tags = merge(var.tags)
+}
+
+resource "aws_cloudwatch_log_group" "flow_log" {
+  name              = "/aws/vpc-flow-log/${local.vpc_id}"
+  retention_in_days = var.flow_log_cloudwatch_log_group_retention_in_days
+  kms_key_id        = var.flow_log_cloudwatch_log_group_kms_key_id
+
+  tags = merge(var.tags)
+}
+
+resource "aws_iam_role" "vpc_flow_log_cloudwatch" {
+  name_prefix        = "vpc-flow-log-role-"
+  assume_role_policy = data.aws_iam_policy_document.flow_log_cloudwatch_assume_role.json
+
+  tags = merge(var.tags)
+}
+
+data "aws_iam_policy_document" "flow_log_cloudwatch_assume_role" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_flow_log_cloudwatch" {
+  role       = aws_iam_role.vpc_flow_log_cloudwatch.name
+  policy_arn = aws_iam_policy.vpc_flow_log_cloudwatch.arn
+}
+
+resource "aws_iam_policy" "vpc_flow_log_cloudwatch" {
+  name_prefix = "vpc-flow-log-to-cloudwatch-"
+  policy      = data.aws_iam_policy_document.vpc_flow_log_cloudwatch.json
+}
+
+data "aws_iam_policy_document" "vpc_flow_log_cloudwatch" {
+  statement {
+    sid = "AWSVPCFlowLogsPushToCloudWatch"
+
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
