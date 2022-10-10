13562fb31e150c375ec457465f6921770d9120f8
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-10 16:37:36 +0100

    manage rules in default security group

--- main.tf
+++ main.tf
@@ -29,3 +29,27 @@ resource "aws_nat_gateway" "gw" {
   subnet_id     = aws_subnet.public[count.index].id
   tags          = { name = "${local.name-prefix}-nat-gateway-${data.aws_availability_zones.all.names[count.index]}" }
 }
+
+
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.main.default_network_acl_id
+
+  # no rules defined, deny all traffic in this ACL
+}
+
+
+# resource "aws_network_acl" "bar" {
+#   vpc_id = aws_vpc.foo.id
+# }
+
+# resource "aws_network_acl_rule" "bar" {
+#   network_acl_id = aws_network_acl.bar.id
+#   rule_number    = 200
+#   egress         = false
+#   protocol       = "tcp"
+#   rule_action    = "allow"
+#   cidr_block     = aws_vpc.foo.cidr_block
+#   from_port      = 22
+#   to_port        = 22
+# }
