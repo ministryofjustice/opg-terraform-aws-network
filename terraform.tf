aebebd7e47c0ff80f2a07efc0fe07c1b62c97861
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-09 10:04:40 +0000

    Update workflow to use shared actions #patch Adjust terraform to use versions.tf file

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.5.0"
-    }
-  }
-}
-
 data "aws_availability_zones" "all" {
   state = "available"
 }
